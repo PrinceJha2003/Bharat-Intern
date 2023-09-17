a9e274fe91cb737294f7264e44f2d37cf427daed
PrinceJha2003 <123474662+PrinceJha2003@users.noreply.github.com>
2023-09-17 15:05:26 +0530

    Add files via upload

---
+++ Task-02-Temperature-Converter/script.js
@@ -0,0 +1,36 @@
+const convertButton = document.getElementById("convertButton");
+convertButton.addEventListener("click", () => {
+    const temperatureInput = document.getElementById("temperature").value;
+    const temperatureOutput = document.getElementById("result");
+    const unit1 = document.getElementById("unit1").value;
+    const unit2 = document.getElementById("unit2").value;
+
+    if (temperatureInput !== "") {
+        const temperature = parseFloat(temperatureInput);
+        if (unit1 === "celsius") {
+            if(unit2==="fahrenheit"){
+                temperatureOutput.value = ((temperature * 9 / 5) + 32).toFixed(2);
+            }else if(unit2==="kelvin"){
+                temperatureOutput.value = (temperature + 273.15).toFixed(2);
+            }else{
+                temperatureOutput.value = temperature.toFixed(2);
+            }
+        } else if (unit1 === "fahrenheit") {
+            if(unit2==="fahrenheit"){
+                temperatureOutput.value = temperature.toFixed(2);
+            }else if(unit2==="kelvin"){
+                temperatureOutput.value = ((temperature - 32) * 5 / 9 + 273.15).toFixed(2);
+            }else{
+                temperatureOutput.value = ((temperature - 32) * 5 / 9).toFixed(2);
+            }
+        } else if (unit1 === "kelvin") {
+            if(unit2==="fahrenheit"){
+                temperatureOutput.value = ((temperature - 273.15) * 9 / 5 + 32).toFixed(2);
+            }else if(unit2==="kelvin"){
+                temperatureOutput.value = (temperature).toFixed(2);
+            }else{
+                temperatureOutput.value = (temperature - 273.15).toFixed(2);
+            }
+        }
+    } 
+});
